cec86e7d89a7c6eefb55ee5a0d77626df01aa79e
Preet0997 <Preet.Shah@studentambassadors.com>
2024-05-17 17:22:11 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/Photo.jpg";
 
-const imageAltText = "Preet Shah: Microsoft Student Ambassador";
+const imageAltText = "Preet Shah: Microsoft Student Ambassadors";
 
 const Home = ({ name, title }) => {
   return (
